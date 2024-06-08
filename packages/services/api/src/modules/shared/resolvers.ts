448087a10e99623e3cf254415d6a906552599cb1
Eddy Nguyen <github@eddeee888.me>
2024-06-08 23:10:23 +1000

    Migrate JSON and JSONSchemaObject resolvers

--- packages/services/api/src/modules/shared/resolvers.ts
+++ packages/services/api/src/modules/shared/resolvers.ts
@@ -1,10 +1,6 @@
-import { DateResolver, DateTimeResolver, JSONResolver, SafeIntResolver } from 'graphql-scalars';
+import { DateResolver, DateTimeResolver, SafeIntResolver } from 'graphql-scalars';
 import type { SharedModule } from './__generated__/types';
 
-// Remove descriptions from resolvers
-// `scalar JSON` in `module.graphql.ts` does not have a description
-// and it messes up the static analysis
-JSONResolver.description = undefined;
 DateTimeResolver.description = undefined;
 SafeIntResolver.description = undefined;
 DateResolver.description = undefined;
@@ -12,7 +8,5 @@ DateResolver.description = undefined;
 export const resolvers: SharedModule.Resolvers = {
   Date: DateResolver,
   DateTime: DateTimeResolver,
-  JSON: JSONResolver,
-  JSONSchemaObject: JSONResolver,
   SafeInt: SafeIntResolver,
 };
