201d8d587b683cf2eb578d97d1ee00bd42714541
Eddy Nguyen <github@eddeee888.me>
2024-06-08 23:16:12 +1000

    Migrate Date and DateTime resolvers

---
+++ packages/services/api/src/modules/shared/resolvers.generated.ts
@@ -0,0 +1,20 @@
+/* This file was automatically generated. DO NOT UPDATE MANUALLY. */
+import { SafeIntResolver } from 'graphql-scalars';
+import type { Resolvers } from './../../__generated__/types.next';
+import { Date } from './resolvers/Date';
+import { DateTime } from './resolvers/DateTime';
+import { JSON } from './resolvers/JSON';
+import { JSONSchemaObject } from './resolvers/JSONSchemaObject';
+import { noop as Mutation_noop } from './resolvers/Mutation/noop';
+import { noop as Query_noop } from './resolvers/Query/noop';
+
+export const resolvers: Resolvers = {
+  Query: { noop: Query_noop },
+  Mutation: { noop: Mutation_noop },
+
+  Date: Date,
+  DateTime: DateTime,
+  JSON: JSON,
+  JSONSchemaObject: JSONSchemaObject,
+  SafeInt: SafeIntResolver,
+};
--- packages/services/api/src/modules/shared/resolvers.ts
+++ packages/services/api/src/modules/shared/resolvers.ts
@@ -1,12 +1,8 @@
-import { DateResolver, DateTimeResolver, SafeIntResolver } from 'graphql-scalars';
+import { SafeIntResolver } from 'graphql-scalars';
 import type { SharedModule } from './__generated__/types';
 
-DateTimeResolver.description = undefined;
 SafeIntResolver.description = undefined;
-DateResolver.description = undefined;
 
 export const resolvers: SharedModule.Resolvers = {
-  Date: DateResolver,
-  DateTime: DateTimeResolver,
   SafeInt: SafeIntResolver,
 };
