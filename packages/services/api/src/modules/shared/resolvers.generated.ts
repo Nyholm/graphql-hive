5d609be31cd5727645ead9ea5041ccf9127faf91
Eddy Nguyen <github@eddeee888.me>
2024-06-08 23:18:35 +1000

    Migrate SafeInt resolver, remove empty resolvers map

--- packages/services/api/src/modules/shared/resolvers.generated.ts
+++ packages/services/api/src/modules/shared/resolvers.generated.ts
@@ -1,5 +1,4 @@
 /* This file was automatically generated. DO NOT UPDATE MANUALLY. */
-import { SafeIntResolver } from 'graphql-scalars';
 import type { Resolvers } from './../../__generated__/types.next';
 import { Date } from './resolvers/Date';
 import { DateTime } from './resolvers/DateTime';
@@ -7,6 +6,7 @@ import { JSON } from './resolvers/JSON';
 import { JSONSchemaObject } from './resolvers/JSONSchemaObject';
 import { noop as Mutation_noop } from './resolvers/Mutation/noop';
 import { noop as Query_noop } from './resolvers/Query/noop';
+import { SafeInt } from './resolvers/SafeInt';
 
 export const resolvers: Resolvers = {
   Query: { noop: Query_noop },
@@ -16,5 +16,5 @@ export const resolvers: Resolvers = {
   DateTime: DateTime,
   JSON: JSON,
   JSONSchemaObject: JSONSchemaObject,
-  SafeInt: SafeIntResolver,
+  SafeInt: SafeInt,
 };
--- packages/services/api/src/modules/shared/resolvers.ts
+++
@@ -1,8 +0,0 @@
-import { SafeIntResolver } from 'graphql-scalars';
-import type { SharedModule } from './__generated__/types';
-
-SafeIntResolver.description = undefined;
-
-export const resolvers: SharedModule.Resolvers = {
-  SafeInt: SafeIntResolver,
-};
