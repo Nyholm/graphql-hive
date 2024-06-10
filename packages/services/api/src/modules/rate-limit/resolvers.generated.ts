29f364d0ed3a6779c1864e7196083bd65ade4c9b
Eddy Nguyen <github@eddeee888.me>
2024-06-10 13:09:32 +1000

    Regen

--- packages/services/api/src/modules/rate-limit/resolvers.generated.ts
+++ packages/services/api/src/modules/rate-limit/resolvers.generated.ts
@@ -1,7 +1,7 @@
 /* This file was automatically generated. DO NOT UPDATE MANUALLY. */
 import type { Resolvers } from './../../__generated__/types.next';
-import { Organization } from './resolvers/Organization';
+import { Organization as rate_limit_Organization } from './resolvers/Organization';
 
 export const resolvers: Resolvers = {
-  Organization: Organization,
+  Organization: rate_limit_Organization,
 };
