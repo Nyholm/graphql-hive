3562c3d53252e15b5628741e8f91dc4676de0f38
Eddy Nguyen <github@eddeee888.me>
2024-06-10 13:34:32 +1000

    Migrate SupportTicket

--- packages/services/api/src/modules/support/resolvers.ts
+++ packages/services/api/src/modules/support/resolvers.ts
@@ -1,6 +1,5 @@
 import { SupportModule } from './__generated__/types';
 import { SupportTicketPriority, SupportTicketStatus } from './../../shared/entities';
-import { SupportManager } from './providers/support-manager';
 
 export const resolvers: SupportModule.Resolvers & {
   SupportTicketPriority: {
@@ -10,29 +9,6 @@ export const resolvers: SupportModule.Resolvers & {
     [K in SupportModule.SupportTicketStatus]: SupportTicketStatus;
   };
 } = {
-  SupportTicket: {
-    async comments(ticket, args, { injector }) {
-      const response = await injector.get(SupportManager).getTicketComments(ticket.id);
-
-      return {
-        edges: response.nodes.map(comment => ({
-          node: {
-            id: String(comment.id),
-            body: comment.body,
-            createdAt: comment.created_at,
-            fromSupport: comment.fromSupport,
-          },
-          cursor: String(comment.id),
-        })),
-        pageInfo: {
-          endCursor: String(response.nodes[response.nodes.length - 1]?.id ?? ''),
-          hasNextPage: response.meta.has_more,
-          hasPreviousPage: false,
-          startCursor: String(response.nodes[0]?.id ?? ''),
-        },
-      };
-    },
-  },
   SupportTicketStatus: {
     OPEN: SupportTicketStatus.OPEN,
     SOLVED: SupportTicketStatus.SOLVED,
